48c983d8cb426dc8a4c88cd193049a016183097a
Dechaoxu <57580358+Dechaoxu@users.noreply.github.com>
2020-10-06 19:10:46 -0500

    Add files via upload

---
+++ implementation-Dechaoxu-master/src/test/java/com/example/project/CharConverterTest.java
@@ -0,0 +1,21 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CharConverterTest {
+  private final CharConverter converter = new CharConverter();
+
+  @Test
+  void convertIntToLetterSucceeds() {
+
+    assertEquals("A", converter.convertIntToLetter(65));
+  }
+
+  @Test
+  void convertIntToLetterBSucceeds() {
+
+    assertEquals("B", converter.convertIntToLetter(66));
+  }
+}
